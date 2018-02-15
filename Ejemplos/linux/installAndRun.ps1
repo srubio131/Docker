23166cc741c740f02956d43bedb0e586da99fb10
Sergio Rubio Anaya <s.rubio131@gmail.com>
2018-02-15 15:10:35 +0100

    Added installAndRun.ps1

--- Ejemplos/linux/installAndRun.ps1
+++ Ejemplos/linux/installAndRun.ps1
@@ -1,10 +1,21 @@
-$existeImagen = docker images | Where-Object {$_ -like "appPHP*"}
+$IMAGE = "myphpapp"
+$VERSION = ":latest"
+$PUERTO = "8000:80"
+
+$existeImagen = docker images | Where-Object { $_ -like "*$($IMAGE)*" }
 # Construir imagen "appPHP" si no existe ya..
 if ([string]::IsNullOrEmpty($existeImagen)) { 
-  docker build -t myphpapp:latest .
-} 
-docker run -d --rm myphpapp:latest ;
+  docker build -t $IMAGE$VERSION .
+}
 
-# Cerrar el contenedor docker para que no se quede hasta el infinito y más allá
-"Parando el contenedor docker" ;
-docker stop myphpapp:latest
+# Lanzar contenedor con esta imagen
+$existeContenedor = docker container ls --filter "status=running" | Where-Object { $_ -like "*$($IMAGE)*" }
+if ([string]::IsNullOrEmpty($existeContenedor)) {
+  docker run -d -p $PUERTO --name $IMAGE --rm $IMAGE$VERSION  
+} else {
+  docker start $IMAGE
+}
+
+
+# Ojo cuidao: Al terminar e usar el contenedor habría que cerrarlo para que no se quede hasta el infinito y más allá
+#docker stop $IMAGE
